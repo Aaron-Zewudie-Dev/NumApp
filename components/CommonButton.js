a19eeb4a48933c59adaeebab1b6fd96620f54dfb
Aaron Z <78126519+azewudie@users.noreply.github.com>
2025-11-10 12:22:48 -0500

    added style for button & text input feild

--- components/CommonButton.js
+++ components/CommonButton.js
@@ -1,9 +1,42 @@
-import { View, Text } from "react-native";
-function CommonButton({buttonLable}) {
+import { View, Text, Pressable, StyleSheet } from "react-native";
+function CommonButton({ buttonLable, onPress }) {
   return (
-    <View>
-      <Text>{buttonLable}</Text>
+    <View style={commonButtonStyle.buttonOuterContainerStyle}>
+      <Pressable
+        onPress={onPress}
+        android_ripple={{ color: "#640233" }}
+        style={(pressed) =>
+          pressed
+            ? [
+                commonButtonStyle.buttonInnerContainerStyle,
+                commonButtonStyle.pressed,
+              ]
+            : commonButtonStyle.buttonInnerContainerStyle
+        }
+      >
+        <Text style={commonButtonStyle.buttonTextStyle}>{buttonLable}</Text>
+      </Pressable>
     </View>
   );
 }
 export default CommonButton;
+const commonButtonStyle = StyleSheet.create({
+  buttonOuterContainerStyle: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainerStyle: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonTextStyle: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
--- screens/home/HomeScreen.js
+++ screens/home/HomeScreen.js
@@ -3,7 +3,12 @@ import CommonButton from "../../components/CommonButton";
 function HomeScreen() {
   return (
     <View style={homeScreenStyle.inputContener}>
-      <TextInput />
+      <TextInput
+        style={homeScreenStyle.inputFieldStyle}
+        maxLength={2}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <CommonButton buttonLable="Rest" />
       <CommonButton buttonLable="Confirm" />
     </View>
@@ -13,11 +18,25 @@ export default HomeScreen;
 const homeScreenStyle = StyleSheet.create({
   inputContener: {
     marginTop: 100,
-    marginHorizontal:24,
-    padding: 16,
-    backgroundColor:"#72063c",
-    borderRadius:8,
-    elevation:10,
-
+    marginHorizontal: 24,
+    padding: 8,
+    backgroundColor: "#4e0329",
+    borderRadius: 8,
+    elevation: 10,
+    shadowColor: "black",
+    shadowOffset: { with: 0, height: 2 },
+    shadowRadius: 8,
+    shadowOpacity: 0.25,
+  },
+  inputFieldStyle: {
+    height: 60,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
