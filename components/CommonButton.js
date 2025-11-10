3b55759fc2057790d9def29fa570d3cd658d773b
Aaron Z <78126519+azewudie@users.noreply.github.com>
2025-11-10 11:37:00 -0500

    added style

---
+++ components/CommonButton.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+function CommonButton({buttonLable}) {
+  return (
+    <View>
+      <Text>{buttonLable}</Text>
+    </View>
+  );
+}
+export default CommonButton;
--- screens/home/HomeScreen.js
+++ screens/home/HomeScreen.js
@@ -1,2 +1,23 @@
-function HomeScreen(){}
+import { View, Text, Button, TextInput, StyleSheet } from "react-native";
+import CommonButton from "../../components/CommonButton";
+function HomeScreen() {
+  return (
+    <View style={homeScreenStyle.inputContener}>
+      <TextInput />
+      <CommonButton buttonLable="Rest" />
+      <CommonButton buttonLable="Confirm" />
+    </View>
+  );
+}
 export default HomeScreen;
+const homeScreenStyle = StyleSheet.create({
+  inputContener: {
+    marginTop: 100,
+    marginHorizontal:24,
+    padding: 16,
+    backgroundColor:"#72063c",
+    borderRadius:8,
+    elevation:10,
+
+  },
+});
